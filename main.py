493bf83833173752936fa8fef068ceaeb2145fba
Ben <benjamin.c.wheeler-1@ou.edu>
2025-04-19 21:02:53 -0500

    Retrieve SUMO_PATH from the sumo library import

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sys
 import traci
 import sumo
 
-SUMO_PATH = os.path.abspath('.venv/lib/python3.13/site-packages/sumo')
-os.environ['SUMO_PATH'] = SUMO_PATH
+SUMO_PATH = os.path.dirname(sumo.__file__)
+#os.environ['SUMO_PATH'] = SUMO_PATH
+print(SUMO_PATH)
 
 traci.start([os.path.join(SUMO_PATH, 'bin/sumo-gui')])
