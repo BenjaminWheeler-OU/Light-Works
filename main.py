727419032cd185a6274ab917455e7c8feabce9f0
Ben <benjamin.c.wheeler-1@ou.edu>
2025-04-20 21:12:30 -0500

    Search for the binary with the .exe extension as well

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import traci
 import sumo
 
 SUMO_PATH = os.path.dirname(sumo.__file__)
-#os.environ['SUMO_PATH'] = SUMO_PATH
-print(SUMO_PATH)
 
-traci.start([os.path.join(SUMO_PATH, 'bin/sumo-gui')])
+sumo_bin = os.path.join(SUMO_PATH, 'bin/sumo-gui')
+
+if os.path.exists(sumo_bin):
+    traci.start([sumo_bin])
+elif os.path.exists(sumo_bin+".exe"):
+    traci.start([sumo_bin+".exe"])
+else:
+    print(f"Could not find sumo binary at: {sumo_bin}", file=sys.stderr)
+    exit(1)
