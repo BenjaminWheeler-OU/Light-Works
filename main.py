e990c36db6ce6feef0c2f703b0cc13c49b302cc1
Ben <benjamin.c.wheeler-1@ou.edu>
2025-04-21 11:17:25 -0500

    Little documentation

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
+
+#Make sure to install requirements.txt with pip:
+#pip install -r requirements.txt
+
+#Virtual environments will also work:
+#https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
+
 import os
 import sys
 import traci
 import sumo
 from sumolib import checkBinary
 
-#SUMO_PATH = os.path.dirname(sumo.__file__)
+#Sumo expects the environment variable SUMO_PATH to be set to the directory of the Sumo installation
+#If there is none, set it to the directory of the Sumo import
+if not "SUMO_PATH" in os.environ:
+    os.environ["SUMO_PATH"] = os.path.dirname(sumo.__file__)
+SUMO_PATH = os.environ["SUMO_PATH"]
 
 sumo_binary = checkBinary('sumo-gui')
 traci.start([sumo_binary])
