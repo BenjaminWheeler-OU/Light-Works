4c92520ccadff865c39ea2491b4ba107c527f451
Ben <benjamin.c.wheeler-1@ou.edu>
2025-04-22 15:45:41 -0500

    Wrap in if name = main

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ import traci
 import sumo
 from sumolib import checkBinary
 
-#Sumo expects the environment variable SUMO_PATH to be set to the directory of the Sumo installation
-#If there is none, set it to the directory of the Sumo import
-if not "SUMO_PATH" in os.environ:
-    os.environ["SUMO_PATH"] = os.path.dirname(sumo.__file__)
-SUMO_PATH = os.environ["SUMO_PATH"]
-
-sumo_binary = checkBinary('sumo-gui')
-traci.start([sumo_binary])
+if __name__ == "__main__":
+    #Sumo expects the environment variable SUMO_PATH to be set to the directory of the Sumo installation
+    #If there is none, set it to the directory of the Sumo import
+    if not "SUMO_PATH" in os.environ:
+        os.environ["SUMO_PATH"] = os.path.dirname(sumo.__file__)
+    SUMO_PATH = os.environ["SUMO_PATH"]
+    
+    sumo_binary = checkBinary('sumo-gui')
+    traci.start([sumo_binary])
