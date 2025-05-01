55c37245d1f3812384dc539704e773736fba819a
Edward Wages <edwardwages27@gmail.com>
2025-04-30 21:29:47 -0500

    First values written to DataFrame

---
+++ export.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import traci
+import sumo
+from sumolib import checkBinary
+import pandas as pd
+import learningAlgorithm
+
+
+#Taking the info through traci, entering it into a pandas DataFrame & interting those into an output excel file
+file_name = 'bestPopulationData.xlsx'
+
+#Best population total waiting time from all generations
+#Average total waiting time after all generation 
+avgTotalTime = learningAlgorithm.bestPopulation.totalWaitingTime / len(learningAlgorithm.bestPopulation.intersections)
+fileData = pd.DataFrame({
+    'Best total waiting time': [learningAlgorithm.bestPopulation.totalWaitingTime],
+    'Avg total waiting time': [avgTotalTime],
+
+    #TODO: Add emissions reduced & profit saved cells
+    'Best estimated total emissions': [learningAlgorithm.emissions_reduced],
+    'Estimated profit saved': [learningAlgorithm.profit_saved]
+    
+})
+
+dataToExcel = pd.ExcelWriter(file_name)
+
+fileData.to_excel(dataToExcel)
+
+dataToExcel.close()
+print('Data was successfully exported to an Excel File.')
